ebef9e3b8040a429e50ac2f09b34c9bfdf7e445a
dkos <kosdejan@yahoo.com>
2020-08-15 12:12:24 +0200

    wip

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,15 @@
-use confy::ConfyError;
-use serde::{Deserialize, Serialize};
 use std::path::Path;
 
-#[derive(Serialize, Deserialize)]
-pub struct ServiceConfig {}
+use confy::ConfyError;
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ServiceConfig {
+    ip: String,
+    port: u16,
+    workers: u8,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DbConfig {
@@ -18,6 +24,21 @@ impl Default for DbConfig {
     }
 }
 
+impl Default for ServiceConfig {
+    fn default() -> Self {
+        ServiceConfig {
+            ip: "localhost".to_string(),
+            port: 8080,
+            workers: num_cpus::get() as u8,
+        }
+    }
+}
+
 pub fn load_db_config() -> Result<DbConfig, ConfyError> {
     confy::load_path(Path::new("./db_config.toml"))
 }
+
+pub fn load_service_config() -> Result<ServiceConfig, ConfyError> {
+    confy::load_path(Path::new("./service_config.toml"))
+}
+
